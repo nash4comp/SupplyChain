30d23c73c39566865e147f4b0b305b39731f811e
NB <nash4comp@gmail.com>
2023-03-14 11:51:26 -0700

    Update main doc comment

--- storefront.py
+++ storefront.py
@@ -1,3 +1,15 @@
+"""
+# TODO: Add title and description
+
+# Name1: Nash Baek (nash4comp@gmail.com)
+# Student number1: A01243888
+
+# Name2: Taylor Ji (taylor.ji719@gmail.com)
+# Student number2: A01304056
+
+UML diagram: https://app.diagrams.net/#G18MWO3bp974lfK4Ceehz2vUfH8YmqEEfE
+"""
+
 def display_menu():
 	user_input = None
 	while user_input != 3:
