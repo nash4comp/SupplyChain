3b58f3a3ec52e400cf84e2de76d0e9ae15b8e202
taylorJ <taylor.ji719@gmail.com>
2023-03-23 00:24:46 -0700

    completed commenting

--- inventory.py
+++ inventory.py
@@ -5,6 +5,12 @@ class Inventory:
         self._inventory_candy = {}
 
     def check_item_quantity_helper(self, item, item_key_dict):
+        """
+        Helps check_item_quantity function to validate the order and its dictionary that has all the required
+        information to create the item, especially the quantity
+        :param item: Product, item to validate
+        :param item_key_dict: dictionary, dictionary that has all the required information to create the item
+        """
         pid = item.get_pid()
         if pid in item_key_dict.keys():
             for pid in self._inventory_toy.keys():
@@ -18,6 +24,12 @@ class Inventory:
             item_key_dict[item.get_pid()] = item
 
     def check_item_quantity(self, spec, factory):
+        """
+        Check the item's quantity and if the item's quantity in the inventory is less than the item to process's quantity
+        then add 100 items, then subtract item quantity to process.
+        :param spec: dictionary
+        :param factory: Factory
+        """
         item_type = spec["item_type"]
         holiday = spec["holiday"]
         if item_type == "Toy":
@@ -100,6 +112,11 @@ class Inventory:
                 self.check_item_quantity_helper(cream_eggs, self.get_item_inventory_dict(item_type))
 
     def get_item_inventory_dict(self, item_type):
+        """
+        Get corresponding dictionary to the item_type
+        :param item_type: String
+        :return: inventory_toy for toy, inventory_stuffed_animal for stuffedAnimal, inventory_toy for candy
+        """
         if item_type == "Toy":
             return self._inventory_toy
         elif item_type == "StuffedAnimal":
@@ -402,12 +419,18 @@ class Inventory:
     #     self.add_item(spec9, factory)  # Cream Eggs
 
     def display_toys(self):
+        """
+        Display each toys inside the inventory_toy
+        """
         for item in self._inventory_toy.keys():
             print(f"{self._inventory_toy[item].get_product_type()}, {self._inventory_toy[item].get_name()}"
                   f", {self._inventory_toy[item].get_quantity()}"
                   f", {self.check_inventory(self._inventory_toy[item].get_quantity())}")
 
     def display_stuffed_animals(self):
+        """
+        Display each toys inside the inventory_stuffed_animals
+        """
         for item in self._inventory_stuffed_animal.keys():
             print(f"{self._inventory_stuffed_animal[item].get_product_type()}, "
                   f"{self._inventory_stuffed_animal[item].get_name()}"
@@ -415,12 +438,18 @@ class Inventory:
                   f", {self.check_inventory(self._inventory_stuffed_animal[item].get_quantity())}")
 
     def display_candy(self):
+        """
+        Display each toys inside the inventory_candy
+        """
         for item in self._inventory_candy.keys():
             print(f"{self._inventory_candy[item].get_product_type()}, {self._inventory_candy[item].get_name()}"
                   f", {self._inventory_candy[item].get_quantity()}"
                   f", {self.check_inventory(self._inventory_candy[item].get_quantity())}")
 
     def display_each_item_inventory(self):
+        """
+        Display all the inventory items with its status
+        """
         print("Inventory Status")
         print("<Toy>")
         self.display_toys()
