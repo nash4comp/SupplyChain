3a9b7b0f4450201497ad883ff16322ac24dde136
NB <nash4comp@gmail.com>
2023-03-23 00:54:16 -0700

    Add top doc string

--- driver.py
+++ driver.py
@@ -1,5 +1,10 @@
 """
-# TODO: Add title and description
+The Supply Chain Management System
+
+This program is a simulation of a supply chain management system. It is a
+command-line program that allows the user to create a gift factory and
+create orders for the factory to fulfill. The program will then process
+the orders and print out the results.
 
 # Name1: Nash Baek (nash4comp@gmail.com)
 # Student number1: A01243888
@@ -8,6 +13,7 @@
 # Student number2: A01304056
 
 UML diagram: https://app.diagrams.net/#G18MWO3bp974lfK4Ceehz2vUfH8YmqEEfE
+Sequence diagram: https://app.diagrams.net/#G1gf1FB-nTLsNcJypwlBpQREfXNScSEKYN
 """
 
 from store_front import StoreFront
